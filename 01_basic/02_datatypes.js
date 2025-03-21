8e72eadd4ac322a3afafeee62943f147af732e85
ankitydv12 <153325875+ankitydv12@users.noreply.github.com>
2025-03-21 16:08:12 +0000

    commiting

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(2+2) 
+// this will give a pop alter in browser but not in vs codde 
+
+
+                            // datatypes
+
+
+let name = "ankit"          // this String data type 
+let age = 19                // this numeric data type it contain both integer and float 
+let isLoggedIn = false      // this boolean datatype
+let state                   // this undefine 
+let it_contain_null = null
+
+
+//String => set of characters
+//Numeric => ""
+//null    => stand alone value 
+//undefine =>  unassign variable are undefine 
+//symbol => unique 
+
+console.log(typeof state); // undefine because it not inititated 
+console.log(typeof it_contain_null); // it return object 
+
