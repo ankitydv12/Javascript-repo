37c3c4ab9d593c075e8c858f95071c36efdf39c6
ankitydv12 <153325875+ankitydv12@users.noreply.github.com>
2025-03-21 15:45:52 +0000

    commited

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+ 
+const pi = 3.143
+// Declaring a constant which can not be futher  change
+
+
+let account_Email = "ankit88@gmail.com"
+// let is used to declare local variable scope variable 
+
+
+var account_pass = "123@123"
+// var is used for global variable scope declaration
+
+
+console.log(pi);
+console.log(account_Email)
+console.log(account_pass);
+/* console.log() is used to print on the console . and it suppose to return something and it return undefine  
+*/
+
+
+console.table([pi,account_Email,account_pass])
+
+//print multiple value at one time  and it return undefine
