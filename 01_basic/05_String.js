a01ff993d0f87b17e22e3459c0446e79993915fc
ankitydv12 <153325875+ankitydv12@users.noreply.github.com>
2025-03-23 19:40:22 +0000

    good night

---
+++ 01_basic/05_String.js
@@ -0,0 +1,10 @@
+let username = "ankit"
+let permission = "sudo"
+
+//String Interpolation method 
+console.log(`username is ${username} and permission is ${permission}`);
+
+//creating string as object
+
+const name = new String("Ankit Yadav")
+console.log(name);
