6cb2d25dfbd2d77a8a3ac7e0eb871cf59360cd59
ankitydv12 <ankitydv9863@gmail.com>
2025-03-27 00:34:35 +0530

    date

---
+++ 01_basic/07_date_and_time.js
@@ -0,0 +1,32 @@
+let mydate = new Date()
+// it take time from 1970 
+
+console.log(typeof mydate); //object 
+
+console.log(mydate.toDateString()) //change the date into readabe foam --- Thu Mar 27 2025
+console.log(mydate.toLocaleString()) // 3/27/2025, 12:11:31 AM
+ 
+// Creating dates 
+console.log(new Date(2012,0,2).toLocaleString());
+//Month start from YYYY,M,DD
+
+console.log(new Date(2012,0,3,9,23).toLocaleString())
+//YYYY,MM,DD,HH,MM
+
+let cusdate = new  Date("2024-01-12")
+/*
+    *YYYY-MM-DD
+        *
+ */
+
+    let newDate = new Date()
+    console.log(newDate);
+    console.log(newDate.getMonth() + 1);
+    console.log(newDate.getDay());
+    
+    // `${newDate.getDay()} and the time `
+    
+    newDate.toLocaleString('default', {
+        weekday: "long",
+        
+    }) //<mdn>
