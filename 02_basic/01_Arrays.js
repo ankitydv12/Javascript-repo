908bac25de184a2606d3353269c5a7944d1374d2
ankitydv12 <ankitydv9863@gmail.com>
2025-03-29 00:43:57 +0530

    dggghg

--- 02_basic/01_Arrays.js
+++ 02_basic/01_Arrays.js
@@ -22,3 +22,46 @@ console.log(myArray[3]);
 //Methods in Array
 console.log(myArray.push("something")) // add to the end of array
 //console.log(myArray.pop()) // pop the last index element
+
+console.log(myArray.pop())
+//pop the last element(something)
+
+console.log(myArray.includes(5))
+// return true if the myarray have 5 
+
+console.log(myArray.indexOf(5))
+//give the index of the element 
+
+
+console.log( myArrays3.join())
+console.log( myArrays3.join('-'))
+console.log( myArrays3.join(" "))
+console.log( myArrays3.join("*"))
+/**
+ * The join() method of Array instances creates and returns a new string by concatenating all of the elements in this array, separated by commas or a specified separator string.
+ * if no parameter are given join is comma (,)is used
+ *          o/p:
+ *              1,2,3,4,5
+                1-2-3-4-5
+                1 2 3 4 5
+                1*2*3*4*5
+  */
+
+
+                //*******  slice and splice*********
+
+console.log(myArray.slice(0,3)); // give a porstion of array and exclucde 3rd element
+console.log(myArray.splice(0,3)); //cut element from 0 index to 3rd index(include 3rd element)
+
+//*********************************DIFF B/W slice and splice***************************** 
+//       slice => No change in the original array
+         //splice => element are splice (remove) from the original array
+// */
+
+
+
+
+
+
+
+
