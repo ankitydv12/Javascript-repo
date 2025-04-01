363965e5530042624a1b39b5eec6e895c74a1ce8
ankitydv12 <ankitydv9863@hmail.com>
2025-04-01 18:50:20 +0530

    json and randomapi , jsonformmated

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -16,4 +16,82 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+//console.log(regularUser.fullname?.userfullname.firstname); // ? check the fullname is exit or not (work as if and else) . This call optional chaining
+
+const obj1 ={1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+//**********************Combing object**************************  */
+        //Method 1 (incorrect)
+const obj3 = {obj1 , obj2}; console.log(obj3); //problem is same  as Arrays . Object inside the object 
+//output { '1': 'a', '2': 'b' } { '3': 'c', '4': 'd' }
+
+
+        //Method 2 (correct)
+const obj4 = Object.assign({},obj1,obj2) //return an object 
+console.log(obj4);
+/**
+ * Syntax = cost returnobj = Object.assing(target ,source)
+ * Here target is an empty object {} and source are obj1 and obj2 (souce can be multiple) 
+ */
+
+        //Method 3 (correct) Most use 
+const obj5 = {...obj1,...obj2}
+
+
+//********************Arrays of Object***********************/
+
+const arrobj = [
+    {
+        id:1,
+        name:"a"
+    },
+    {
+        id:2,
+        name:"b"
+    },
+    {
+        id:3,
+        name:"c"
+    }
+]
+
+
+console.log(arrobj[0].id);
+//can be acces by index
+
+//***********Some Method of Object class*********/
+
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+console.log(tinderuser.hasOwnProperty('id'))
+
+
+
+//************************Destructuring********************** */
+console.log("************************Destructuring********************** *");
+
+const course = {
+    courname : "js hindi",
+    price:"99",
+    courseInst : "Hitesh"
+}
+
+// course.courname
+
+const {courseInst:Instructor} = course
+console.log(Instructor) 
+
+
+console.log("************************APIs********************** *");
+
+/**
+ * Value come in foam of JSON
+ */
+
+// {
+//     "name":"ankit",
+//     "coursename":"js hindi",
+//     "price" : "free"
+// }
+//this is JSon , * all the value key and value are in string 
